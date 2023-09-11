bb02a6108f5719de2211539937e777147f23acd4
Tyhe Ferenc <tyhe444@gmail.com>
2023-09-11 17:00:45 +0200

    index.js changes

--- index.js
+++ index.js
@@ -1,46 +1,68 @@
 const prompt = require("prompt-sync")({ sigint: true });
-console.log("**WELCOME TO THE UNDERWATER STORY**");
 
-let playerLife = 2;
-console.log(`Breathe: ${"* ".repeat(playerLife)}`);
-
-function lifeBar() {
-  return playerLife;
+function displayLife(playerLife) {
+  return "* ".repeat(playerLife);
 }
 
-function question() {
-  lifeBar();
+function askQuestion(question, choices, correctChoice) {
+  console.log(question);
+  for (let i = 0; i < choices.length; i++) {
+    console.log(`${i + 1}. ${choices[i]}`);
+  }
 
-  let firstQuestion = prompt("How mutch is 1 + 1?");
-  if (firstQuestion === "2") {
-    playerLife++;
-    console.log(
-      `Correct! The answer is ${firstQuestion} Lets go to the next question!`
-    );
+  const userChoice = parseInt(prompt("Your choice: "));
+
+  if (isNaN(userChoice) || userChoice < 1 || userChoice > choices.length) {
+    console.log("Please enter a valid choice.");
+    return false;
+  }
+
+  if (userChoice === correctChoice) {
+    console.log(`Correct!`);
+    return true;
   } else {
-    playerLife--;
-    console.log("Wrong answer");
+    console.log("Wrong choice.");
+    return false;
+  }
+}
+
+function playGame() {
+  console.log("**WELCOME TO THE UNDERWATER STORY**");
+
+  let playerLife = 4;
+  console.log(`Breathe: ${displayLife(playerLife)}`);
+
+  const questions = [
+    {
+      question: "You find a mysterious door. What do you do?",
+      choices: ["Open it", "Leave it alone"],
+      correctChoice: 1, 
+    },
+    {
+      question: "Inside the door, you see a treasure chest. How do you proceed?",
+      choices: ["Open the chest", "Walk away"],
+      correctChoice: 0, 
+    },
+    {
+
+    }
+  ];
+
+  for (const { question, choices, correctChoice } of questions) {
+    if (!askQuestion(question, choices, correctChoice)) {
+      playerLife--;
+      console.log(`Breathe: ${displayLife(playerLife)}`);
+      if (playerLife === 0) {
+        console.clear();
+        console.log("Game over");
+        return;
+      }
+    }
   }
 
   console.clear();
-  console.log(`Breathe: ${"* ".repeat(playerLife)}`);
-  let secondQuestion = prompt("How mutch us 2 + 5?");
-  if (secondQuestion === "7") {
-    playerLife++;
-    console.log(`Breathe: ${"* ".repeat(playerLife)}`);
-    console.log(
-      `Correct! The answer is ${secondQuestion} Lets go to the next question!`
-    );
-  } else {
-    playerLife--;
-    console.log(`Breathe: ${"* ".repeat(playerLife)}`);
-    console.log("Wrong answer");
-  }
-  if (playerLife === 0) {
-    console.clear();
-    console.log("Game over");
-    return;
-  }
+  console.log("Congratulations! You've completed the game.");
 }
 
-question();
+playGame();
+
